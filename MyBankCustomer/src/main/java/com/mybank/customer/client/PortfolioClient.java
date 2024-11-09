68a3183afa0f39db3ee01734119748e600ebc85d
Leonardo Saldaña <lpsaldana1@gmail.com>
2024-11-09 00:13:49 -0500

    gateway configurations

--- MyBankCustomer/src/main/java/com/mybank/customer/client/PortfolioClient.java
+++ MyBankCustomer/src/main/java/com/mybank/customer/client/PortfolioClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient(name = "msvc-mb-customer-portfolio", url = "localhost:8082/api/account")
+@FeignClient(name = "msvc-mb-customer-portfolio", url = "localhost:9080/api/account")
 public interface PortfolioClient {
     @GetMapping("/customer-acc/{id}")
     List<AccountDTO> findAllCustomerAccounts(@PathVariable Long id);
